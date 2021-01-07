10628163decea5e76a6570952d3b6e4baf79c702
ossono <33455708+ossono@users.noreply.github.com>
2021-01-07 11:50:15 +0000

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -57,6 +57,7 @@ docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /va
 #sh -c "echo '/dev/sdb /home/ossono/docker/hassio/share/usb' >> /etc/fstab"
 chmod -R a+rwx /home/ossono/docker
 docker network create znet  # for zoneminder
+wget -q -O - https://hacs.xyz/install | bash -
 #wget -P /home/ossono/ser/h1 https://github.com/hacs/integration/releases/download/1.9.0/hacs.zip
 #wget https://github.com/hacs/integration/releases/download/1.9.0/hacs.zip
 #unzip hacs.zip -d /home/ossono/docker/hassio/homeassistant/custom_components/hacs
