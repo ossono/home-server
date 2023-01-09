1331a2b32d6a6826123a89cf6bb7478513a06819
ossono <33455708+ossono@users.noreply.github.com>
2023-01-09 16:22:40 +0000

    Update inshnew.sh

--- inshnew.sh
+++ inshnew.sh
@@ -14,10 +14,9 @@ network-manager \
 dbus \
 lsb-release \
 systemd-journal-remote -y
-sh -c "echo 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' >> /root/.bashrc"
 curl -fsSL get.docker.com | sh
 wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_x86_64.deb
 dpkg -i os-agent_1.4.1_linux_x86_64.deb
 wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
 dpkg -i homeassistant-supervised.deb
-wget -O - https://get.hacs.xyz | bash -
+
